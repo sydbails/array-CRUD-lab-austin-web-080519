8c14726e7dbf125cdf83806fb492bd7c0963ff53
Syd Bailey <sydabailey@gmail.com>
2019-07-29 04:24:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+["me", "you", "friends", "family"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array ["me", "you"]
+element = "Syd"
+add_element_to_end_of_array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
